742260631f40c225590488652337dd543a44dd68
Aaron C Robinson <>
2017-10-14 06:14:15 -0400

    Bug Fix

--- Source/NoFirewatcher/HarmonyPatches.cs
+++ Source/NoFirewatcher/HarmonyPatches.cs
@@ -14,7 +14,7 @@ namespace NoFirewatcher
 #endif
             HarmonyInstance harmony = HarmonyInstance.Create("rimworld.whyisthat.nofirewatcher.main");
 
-            harmony.Patch(AccessTools.Method(typeof(FireWatcher), nameof(FireWatcher.LargeFireDangerPresent)), new HarmonyMethod(typeof(HarmonyPatches), nameof(DoNothingDetour)), null);
+            harmony.Patch(AccessTools.Property(typeof(FireWatcher), nameof(FireWatcher.LargeFireDangerPresent)).GetGetMethod(), new HarmonyMethod(typeof(HarmonyPatches), nameof(DoNothingDetour)), null);
             harmony.Patch(AccessTools.Method(typeof(FireWatcher), nameof(FireWatcher.FireWatcherTick)), new HarmonyMethod(typeof(HarmonyPatches), nameof(DoNothingDetour)), null);
         }
 
